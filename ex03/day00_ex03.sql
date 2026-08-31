6437aaa025f1fd59f19f46c9fb742788e7cef646
Pluiek3 <pluiek.x3@gmail.com>
2026-08-31 22:06:48 +0300

    exercise 03

--- ex03/day00_ex03.sql
+++ ex03/day00_ex03.sql
@@ -1,1 +1,8 @@
-SELECT DISTINCT person_id FROM person_visits WHERE visit_date = '2022-01-06' or visit_date = '2022-01-07' or visit_date = '2022-01-08' or visit_date = '2022-01-09' or pizzeria_id = 2 ORDER BY person_id DESC;
+SELECT DISTINCT person_id 
+FROM person_visits 
+WHERE visit_date = '2022-01-06' 
+    or visit_date = '2022-01-07' 
+    or visit_date = '2022-01-08' 
+    or visit_date = '2022-01-09' 
+    or pizzeria_id = 2 
+ORDER BY person_id DESC;
